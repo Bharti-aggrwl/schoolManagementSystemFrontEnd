58f398316ed2324b5e240cc4a39d69631ad7aa66
bhartiaggarwal123 <baggarwal592@gmail.com>
2024-02-23 21:34:24 +0530

    resoled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule , CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RegistrationComponent } from './components/registration/registration.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -25,7 +25,7 @@ import {MatDividerModule} from '@angular/material/divider';
     LoginComponent,
     HomeComponent,
     DashboardComponent,
-    RegistrationComponent 
+  RegistrationComponent
     
   ],
   imports: [
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatIconModule} from '@angular/material/icon';
+
 
 
 @Component({
