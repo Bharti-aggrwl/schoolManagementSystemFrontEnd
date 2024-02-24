608e86d6251f1fd3f13f94b1cbe2c13db0e1dcdf
shivanisrivastava26 <shivani.sri140@gmail.com>
2024-02-24 12:52:30 +0530

    loginNew

---
+++ src/app/components/login-new/login-new.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-new',
+  templateUrl: './login-new.component.html',
+  styleUrls: ['./login-new.component.css']
+})
+export class LoginNewComponent implements OnInit {
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
+
+}
