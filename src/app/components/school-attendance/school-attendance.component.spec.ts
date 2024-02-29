e8b9b3ef9197a67475db5a336a41ee26714a999e
Ansh-Kataria <anshkataria28@gmail.com>
2024-02-29 20:48:56 +0530

    dashboard updated

---
+++ src/app/components/school-attendance/school-attendance.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SchoolAttendanceComponent } from './school-attendance.component';
+
+describe('SchoolAttendanceComponent', () => {
+  let component: SchoolAttendanceComponent;
+  let fixture: ComponentFixture<SchoolAttendanceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SchoolAttendanceComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SchoolAttendanceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
